390ca585cea4a009afd5a1217542d0c08030de50
Rafe Colton <r.colton@modcloth.com>
2013-12-17 11:25:23 -0800

    Making it easier to simulate running the cli for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,13 @@ def tmpdir(&block)
     yield tmp
   end
 end
+
+def run(cmd = [])
+  if cmd.class == String
+    Dockerize::Cli.run(cmd.split(' '))
+  elsif cmd.class == Array
+    Dockerize::Cli.run(cmd)
+  else
+    fail 'Invalid command'
+  end
+end
